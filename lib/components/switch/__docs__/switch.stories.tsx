9afc865e049a07beff1b7b2f81b3c5aa3fd64a4f
Pau Velasco <pauvelascogarrofe@gmail.com>
2024-08-04 22:12:23 +0200

    add Switch component with Storybook stories

---
+++ lib/components/switch/__docs__/switch.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Switch from "../switch";
+
+const meta: Meta<typeof Switch> = {
+  title: "Switch",
+  component: Switch,
+};
+
+export default meta;
+type Story = StoryObj<typeof Switch>;
+
+export const Basic: Story = {
+  args: {},
+};
+
+export const Advanced: Story = {
+  args: {
+    variant: "advanced",
+  },
+};
+
+export const WithText: Story = {
+  args: {
+    selectedOption: "On",
+    unselectedOption: "Off",
+  },
+};
+
+export const WithLongText: Story = {
+  args: {
+    selectedOption: "First option",
+    unselectedOption: "Second option",
+  },
+};
+
+export const WithLabelText: Story = {
+  decorators: [
+    (Story) => (
+      <>
+        <label htmlFor="switch">Switch button label</label>
+        <Story />
+      </>
+    ),
+  ],
+  args: {
+    selectedOption: "First option",
+    unselectedOption: "Second option",
+    id: "switch",
+  },
+};
