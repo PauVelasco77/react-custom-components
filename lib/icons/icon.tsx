b0dc6bda0703bc753e1b85ed8ff43c42e71ea068
Pau Velasco <pauvelascogarrofe@gmail.com>
2024-09-15 19:33:42 +0200

    add Icon component for displaying SVG icons

---
+++ lib/icons/icon.tsx
@@ -0,0 +1,28 @@
+import { memo } from "react";
+import * as Icons from "./index";
+
+type IconProps = React.SVGProps<SVGSVGElement> & {
+  size?: number | "auto";
+  iconName: keyof typeof Icons;
+};
+
+const Icon = memo(function Icon({
+  size = "auto",
+  iconName,
+  ...props
+}: IconProps): JSX.Element {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      aria-hidden="true"
+      width={size}
+      height={size}
+      {...props}
+    >
+      {Icons[iconName]()}
+    </svg>
+  );
+});
+
+export default Icon;
