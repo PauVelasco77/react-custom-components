e48ed54f12bb2efb076d80ec3e61a2cd06cd5594
Pau Velasco <pauvelascogarrofe@gmail.com>
2024-10-02 00:09:14 +0200

    add ChevronBarDown, ChevronBarLeft, and ChevronBarUp components

--- lib/icons/index.ts
+++ lib/icons/index.ts
@@ -2,3 +2,7 @@ export * from "./components/chevronDown";
 export * from "./components/chevronLeft";
 export * from "./components/chevronRight";
 export * from "./components/chevronUp";
+export * from "./components/chevronBarDown";
+export * from "./components/chevronBarLeft";
+export * from "./components/chevronBarRight";
+export * from "./components/chevronBarUp";
