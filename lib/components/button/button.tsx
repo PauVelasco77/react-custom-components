986b7df355ac64d651ed6d1b18f6036d526c5ad6
PauVelasco77 <80649455+PauVelasco77@users.noreply.github.com>
2024-10-08 09:51:01 +0100

    refactor button component and styles, add new variants and sizes

--- lib/components/button/button.tsx
+++ lib/components/button/button.tsx
@@ -1,15 +1,56 @@
 import styles from "./button.module.css";
+import cx from "classnames";
 
-type ButtonProps = React.ComponentPropsWithoutRef<"button"> & {
+type ButtonVariants =
+  | "primary"
+  | "secondary"
+  | "destructive"
+  | "outline"
+  | "ghost"
+  | "link";
+
+type ButtonSizes = "small" | "medium" | "large" | "icon";
+
+export type ButtonProps = React.ComponentPropsWithoutRef<"button"> & {
   children: React.ReactNode;
+  /**
+   * The variant of the button
+   * @default primary
+   * @example <Button variant="secondary">Secondary</Button>
+   * @type "primary" | "secondary" | "destructive" | "outline" | "ghost" | "link"
+   */
+  variant?: ButtonVariants;
+  /**
+   * The size of the button
+   * @default medium
+   * @example <Button size="small">Small</Button>
+   * @type "small" | "medium" | "large" | "icon"
+   */
+  size?: ButtonSizes;
+  /**
+   * Render the button as a span element
+   * @default false
+   * @example <Button asChild>Child</Button>
+   * @type boolean
+   */
+  asChild?: boolean;
 };
 
 export default function Button({
   children,
+  variant = "primary",
+  size = "medium",
+  asChild = false,
   ...rest
 }: ButtonProps): JSX.Element {
+  const className = cx(styles.button, styles[variant], styles[size]);
+
+  if (asChild) {
+    return <span className={className}>{children}</span>;
+  }
+
   return (
-    <button className={styles.button} {...rest}>
+    <button className={className} {...rest}>
       {children}
     </button>
   );
